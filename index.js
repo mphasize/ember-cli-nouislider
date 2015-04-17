b7ea54825c7de4018cfb7eccb3f03f11257a6e92
Marcus Paeschke <marcus@mphasize.de>
2015-04-17 16:43:42 +0200

    Fix css imports

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ module.exports = {
     this._super.included(app);
 
     app.import(app.bowerDirectory + '/nouislider/distribute/jquery.nouislider.all.js');
-    app.import(app.bowerDirectory + '/nouislider/distribute/jquery.nouislider.min.css');
+    app.import(app.bowerDirectory + '/nouislider/src/jquery.nouislider.css');
+    app.import(app.bowerDirectory + '/nouislider/src/jquery.nouislider.pips.css');
   }
 };
